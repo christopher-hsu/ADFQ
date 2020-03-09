33b3d02e346c0e8ee13ea122685a7ab0c501cd6f
Christopher Hsu <christoherhsu8@gmail.com>
2020-03-09 19:21:34 -0400

    lgos

--- build-docker.sh
+++ build-docker.sh
@@ -2,13 +2,12 @@
 
 docker build -t tracking:latest .
 
-mkdir /home/$(whoami)/logs
 # DATE=$( date +%N )
 docker run --name tracking -it --rm \
     -p 8888:8888 -p 6006:6006 \
     -e DISPLAY=${DISPLAY} \
     --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix \
     --mount type=bind,src=/home/$(whoami)/repositories,dst=/tf \
-    --mount type=bind,src=/home/$(whoami)/logs,dst=/tf \
+    --mount type=bind,src=/home/$(whoami)/logs,dst=/tf/logs \
     -w /tf \
     tracking:latest
