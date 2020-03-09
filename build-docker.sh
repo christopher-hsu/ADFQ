b4d61d9dee085c7ad577dc3b29201e61f84ca8ce
Christopher Hsu <christoherhsu8@gmail.com>
2020-03-09 15:40:55 -0400

    docker

--- build-docker.sh
+++ build-docker.sh
@@ -3,8 +3,8 @@
 docker build -t tracking:latest .
 
 # DATE=$( date +%N )
-docker run --name tracking --shm-size=5G -it --rm \
-	--gpus all \
+docker run --name tracking -it --rm \
+	--gpus device=2 \
 	-p 8888:8888 -p 6006:6006 \
 	-e DISPLAY=${DISPLAY} \
 	--mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix \
