fead68fcc35c114d53618a269ba9c9a363326f3e
Christopher Hsu <christoherhsu8@gmail.com>
2020-03-10 12:42:54 -0400

    docker

--- build-docker.sh
+++ build-docker.sh
@@ -4,7 +4,7 @@ docker build -t tracking:latest .
 
 # DATE=$( date +%N )
 docker run --name tracking -it --rm \
-    --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=1 \
+    --gpus '"device=1"'
     -p 8888:8888 -p 6006:6006 \
     -e DISPLAY=${DISPLAY} \
     --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix \
